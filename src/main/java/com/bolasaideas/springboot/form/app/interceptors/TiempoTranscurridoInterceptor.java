9d1d063adbb10dec5eb729ceaf072a52a94b75d3
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-19 11:09:59 -0500

    Registrando interceptor en la configuración de spring

--- src/main/java/com/bolasaideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolasaideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -8,10 +8,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component
+@Component("tiempoTranscurridoInterceptor")
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 
 	private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
@@ -20,7 +21,12 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		// TODO Auto-generated method stub
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod method = (HandlerMethod) handler;
+			logger.info("Es un metodo del controlador " + method.getMethod().getName());
+		}
 		logger.info("TiempoTranscurridoInterceptor: Entrando al metodo preHandle....");
+		logger.info("Interceptando: " + handler);
 		long tiempoInicio = System.currentTimeMillis();
 		request.setAttribute("tiempoInicio", tiempoInicio);
 
@@ -38,11 +44,11 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor {
 		long tiempoFin = System.currentTimeMillis();
 		long tiempoInicio = (Long) request.getAttribute("tiempoInicio");
 		long tiempoTranscurrido = tiempoFin - tiempoInicio;
-		if (modelAndView != null) {
+		if (modelAndView != null && handler instanceof HandlerMethod) {
 			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
 		}
 
 		logger.info("Tiempo Transcurrido: " + tiempoTranscurrido + " milisisegundos");
-		logger.info("TiempoTranscurridoInterceptor: Entrando al metodo postHandle....");
+		logger.info("TiempoTranscurridoInterceptor: Saliendo del metodo postHandle....");
 	}
 }
